0f27ef31bf1e3c0b1f6213d857d538d4772ac094
Carlos M. Cabrera <243419+pointtonull@users.noreply.github.com>
2019-02-20 17:14:05 -0300

    ipython wrapper that loads files and sets the enviroment

---
+++ src/terminal.py
@@ -0,0 +1,29 @@
+import sys
+
+import IPython
+
+def _main():
+
+    print("Starting interative terminal")
+
+    noise = globals().keys() | locals().keys()
+
+    from lib import image
+    from lib import interface
+    from lib import minimize
+    from lib.interface import showimage
+    import main
+    import numpy as np
+
+    libs = (globals().keys() | locals().keys()) - noise
+    print(libs)
+
+    # Interative terminal
+    terminal = IPython.terminal.embed.InteractiveShellEmbed()
+    terminal.extension_manager.load_extension("autoreload")
+    terminal.run_line_magic("autoreload", "2")
+    terminal.mainloop()
+
+
+if __name__ == "__main__":
+    sys.exit(_main())
