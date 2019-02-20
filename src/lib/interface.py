963bf5159b6d56c4ee3a96f358014660ebce4551
Carlos M. Cabrera <243419+pointtonull@users.noreply.github.com>
2019-02-20 17:16:01 -0300

    not a gui... but

---
+++ src/lib/interface.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+
+from io import StringIO
+import os
+import sys
+
+
+NP = False
+try:
+    import numpy as np
+    NP = True
+except:
+    pass
+
+ndarray = not NP or np.ndarray
+
+
+if os.name in ("nt") or "TKPIPE" in os.environ:
+    import tkpipe
+    import Image as pil
+    TKPIPE = tkpipe.Tkpipe()
+    sys.stdout = TKPIPE.default("green")
+    sys.stderr = TKPIPE.default("red")
+else:
+    from matplotlib import pyplot as plt
+    TKPIPE = False
+
+
+def fig2raster(figure):
+    """
+    Convert a matplotlib to a raster PIL image
+    """
+    if hasattr(figure, "savefig"):
+        fileo = StringIO()
+        figure.savefig(fileo)
+        fileo.seek(0)
+        figure = pil.open(fileo)
+    return figure
+
+
+def showimage(image, title=None):
+    image = fig2raster(image)
+    if TKPIPE:
+        raise NotImplementedError("Must print title!")
+        if isinstance(image, ndarray):
+            try:
+                image = pil.fromarray(image)
+            except TypeError:
+                image = pil.fromarray(np.float64(image))
+            except IndexError:
+                print(image)
+                raise
+        TKPIPE.writeimage(image)
+    else:
+        try:
+            plot = plt.imshow(image)
+        except TypeError:
+            plot = plt.imshow(image.astype(float))
+        plot.set_cmap('gray')
+        plot.axes.set_title(title)
+        plot.axes.set_axis_off()
+        plot.axes.margins(0)
+        return plt.show()
+
+
+def color(message, color="blue"):
+    if TKPIPE:
+        TKPIPE.write(message, color)
+    else:
+        sys.stderr.write(message)
+    return message
+
+
+blue = lambda message:color(message, "blue")
+red = lambda message:color(message, "red")
+green = lambda message:color(message, "green")
+
