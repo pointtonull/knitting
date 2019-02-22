fd1af3b5475cb1c915aa6d2ebc8a0211ea0297d1
Carlos M. Cabrera <243419+pointtonull@users.noreply.github.com>
2019-02-21 22:03:35 -0300

    it'll became a test suite. Soon.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
 import sys
 
+import numpy as np
+
+from lib.interface import showimage
+from lib.image import draw_line
+
+
 def main():
     print("Main function")
+    img = np.ones((11, 11))
+    draw_line(0, 0, 10, 10, img)
+    draw_line(0, 10, 10, 0, img)
+    draw_line(0, 10, 10, 0, img)
+    draw_line(0, 10, 10, 0, img)
+    draw_line(0, 5, 10, 5, img)
+    draw_line(0, 5, 10, 5, img)
+    draw_line(0, 5, 10, 5, img)
+    draw_line(5, 0, 5, 10, img)
+    print(img)
+    showimage(img, "AA Lines with partial opacity")
+
 
 if __name__ == "__main__":
     sys.exit(main())
