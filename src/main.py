9b139fcb98424a3a801725a56c98037214e83e90
Carlos M. Cabrera <243419+pointtonull@users.noreply.github.com>
2019-02-22 01:07:33 -0300

    adjunted to new signature

--- src/main.py
+++ src/main.py
@@ -8,16 +8,21 @@ from lib.image import draw_line
 
 def main():
     print("Main function")
-    img = np.ones((11, 11))
-    draw_line(0, 0, 10, 10, img)
-    draw_line(0, 10, 10, 0, img)
-    draw_line(0, 10, 10, 0, img)
-    draw_line(0, 10, 10, 0, img)
-    draw_line(0, 5, 10, 5, img)
-    draw_line(0, 5, 10, 5, img)
-    draw_line(0, 5, 10, 5, img)
-    draw_line(5, 0, 5, 10, img)
+#     img = np.ones((11, 11))
+    img = np.zeros((11, 11))
+    img[5, 5] = 1
+
+    draw_line(img,  (0,  0),  (10,  10), value=.5, opacity=.01)
+    draw_line(img,  (0,  10), (10,  0),  value=.5, opacity=.01)
+    draw_line(img,  (0,  10), (10,  0),  value=.5, opacity=.01)
+    draw_line(img,  (0,  10), (10,  0),  value=.5, opacity=.01)
+    draw_line(img,  (0,  5),  (10,  5),  value=.5, opacity=.01)
+    draw_line(img,  (0,  5),  (10,  5),  value=.5, opacity=.01)
+    draw_line(img,  (0,  5),  (10,  5),  value=.5, opacity=.01)
+    draw_line(img,  (5,  0),  (5,   10), value=.5, opacity=.01)
+
     print(img)
+    print(img.ptp())
     showimage(img, "AA Lines with partial opacity")
 
 
